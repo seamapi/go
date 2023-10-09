7d45638774b113597bf4d9439357bff5381b269d
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2023-10-09 19:17:19 +0000

    Release v0.1.13

--- thermostats_update_request_default_climate_setting.go
+++ thermostats_update_request_default_climate_setting.go
@@ -3,12 +3,12 @@
 package api
 
 type ThermostatsUpdateRequestDefaultClimateSetting struct {
-	AutomaticHeatingEnabled   *bool                                                         `json:"automatic_heating_enabled,omitempty"`
-	AutomaticCoolingEnabled   *bool                                                         `json:"automatic_cooling_enabled,omitempty"`
-	HvacModeSetting           *ThermostatsUpdateRequestDefaultClimateSettingHvacModeSetting `json:"hvac_mode_setting,omitempty"`
-	CoolingSetPointCelsius    *float64                                                      `json:"cooling_set_point_celsius,omitempty"`
-	HeatingSetPointCelsius    *float64                                                      `json:"heating_set_point_celsius,omitempty"`
-	CoolingSetPointFahrenheit *float64                                                      `json:"cooling_set_point_fahrenheit,omitempty"`
-	HeatingSetPointFahrenheit *float64                                                      `json:"heating_set_point_fahrenheit,omitempty"`
-	ManualOverrideAllowed     *bool                                                         `json:"manual_override_allowed,omitempty"`
+	AutomaticHeatingEnabled   *bool            `json:"automatic_heating_enabled,omitempty"`
+	AutomaticCoolingEnabled   *bool            `json:"automatic_cooling_enabled,omitempty"`
+	HvacModeSetting           *HvacModeSetting `json:"hvac_mode_setting,omitempty"`
+	CoolingSetPointCelsius    *float64         `json:"cooling_set_point_celsius,omitempty"`
+	HeatingSetPointCelsius    *float64         `json:"heating_set_point_celsius,omitempty"`
+	CoolingSetPointFahrenheit *float64         `json:"cooling_set_point_fahrenheit,omitempty"`
+	HeatingSetPointFahrenheit *float64         `json:"heating_set_point_fahrenheit,omitempty"`
+	ManualOverrideAllowed     *bool            `json:"manual_override_allowed,omitempty"`
 }
