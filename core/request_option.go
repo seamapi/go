0065e00f2a734f15b733aed25cade658c1525a5e
fern-api <115122769+fern-api[bot]@users.noreply.github.com>
2024-03-05 01:31:12 +0000

    SDK regeneration

--- core/request_option.go
+++ core/request_option.go
@@ -58,7 +58,7 @@ func (r *RequestOptions) cloneHeader() http.Header {
 	headers := r.HTTPHeader.Clone()
 	headers.Set("X-Fern-Language", "Go")
 	headers.Set("X-Fern-SDK-Name", "github.com/seamapi/go")
-	headers.Set("X-Fern-SDK-Version", "v0.2.15")
+	headers.Set("X-Fern-SDK-Version", "v0.2.16")
 	return headers
 }
 
